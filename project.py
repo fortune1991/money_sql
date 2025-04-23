aa645c89d0c7305760266a9e0421cf0717af9736
fortune1991 <172883547+fortune1991@users.noreply.github.com>
2025-04-23 15:40:13 +0530

    Publish

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import datetime, os, sqlite3
 from project_classes import User, Vault, Pot, Transaction
-from project_functions import submit_transaction, print_slow, int_validator, collect_date, convert_date, summary, create_pot, create_user, create_vault, create_profile, instructions, re_vaults, re_pots, re_transactions, count_pots, count_transactions, count_vaults
+from project_functions import submit_transaction, print_slow, print_slow_nospace, int_validator, collect_date, convert_date, summary, create_pot, create_user, create_vault, create_profile, instructions, re_vaults, re_pots, re_transactions, count_pots, count_transactions, count_vaults
 from time import sleep
 from database import create_database
 
@@ -11,39 +11,31 @@ def main():
     
     # check if database exists
     database_exists = os.path.isfile("/Users/michaelfortune/Developer/projects/money/money_sql/money.db")
-    
     if not database_exists:
-
         create_database()
         print_slow("""
-Welcome to Money Pots, your savings and budgeting calculator.
-        """)
+Welcome to Money Pots, your savings and budgeting calculator.""")
         print_slow(instructions())
         user, vaults, pots = create_profile()
     
     if database_exists:
-
         # Establish a connection to the Database
         db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
         con = sqlite3.connect(db_path)
         cur = con.cursor()
-
         #log user in
         while True:
             print_slow("""
-Welcome to Money Pots, your savings and budgeting calculator. Let me help you to login and view your profile. What's your username?\n\n""")
+Welcome to Money Pots, your savings and budgeting calculator. Let me help you to login and view your profile. What's your username?""")
             login = input().strip() # Remove trailing white space
             user_exists = False
-
             # SQL QUERY TO DETERMINE IF USER EXISTS
             res = cur.execute("SELECT username FROM users")
             returned_users = res.fetchall()
             for user in returned_users:
                 if login == user[0]:
                     user_exists = True
-            
            # REINSTANTIATE OBJECTS FROM DATABASE
-                        
             if user_exists == True:
                 #reinstantiate user
                 user = create_user(login)
@@ -57,10 +49,8 @@ Welcome to Money Pots, your savings and budgeting calculator. Let me help you to
                 returned_transactions = res.fetchall()
                 if len(returned_transactions) > 0:
                     transaction_exists = True
-
                 if not transaction_exists:
                     pass
-
                 else:
                     transactions, transaction_ids = re_transactions(pots, pot_ids, user)
 
@@ -70,19 +60,13 @@ Welcome to Money Pots, your savings and budgeting calculator. Let me help you to
                 
                 for vault in vaults.values():
                     vault.vault_value()
-
                 break
 
             else:
-                print()
-                print("User doesn't exist. Respond 'Try again' 'New user' or 'Exit'")    
-                print()
-
+                print_slow("\nUser doesn't exist. Respond 'Try again' 'New user' or 'Exit'")    
                 response = input()
-
                 if response == "New user":
-                    print()
-                    print("Excellent. Please answer the following questions to create a new user profile")
+                    print_slow("\nExcellent. Please answer the following questions to create a new user profile")
                     user, vaults, pots = create_profile()
                     break
                 elif response == "Try again":
@@ -90,39 +74,26 @@ Welcome to Money Pots, your savings and budgeting calculator. Let me help you to
                 elif response == "Exit":
                     exit()
                 else:
-                    print("Unknown Command. Please try to login again")                
+                    print_slow("Unknown Command. Please try to login again")                
      
     # Loop Menu until exit
-
     while True:
         print_slow('\nNow, I await your commands to proceed. Please type: \n\n "Transaction" to submit a new transaction, \n "Summary" to get a report of vault/pot values, or \n "Instructions" to get a further information on how to use Money Pots \n "Vault" to create a new vault \n "Pot" to create a new pot \n "Exit" to terminate the programme')
-        print()
-        print()
         action = input()
-
         if action == "Transaction":
-    
         # Submit Transactions
-
-            print_slow("Excellent. Now, let me help you create a new transaction.")
-            print()
+            print_slow("\nExcellent. Now, let me help you create a new transaction.")
             transactions = {}
             no_transactions = 1
-            
             while True:
                 try:
                     for x in range(no_transactions):
-                        print(f"Transaction {x+1}")
-                        print()
-                        
+                        print_slow(f"Transaction {x+1}")
                         while True: 
-                              
                             # Count existing transactions
                             start_transaction = count_transactions()
-                            
                             print_slow("What pot should this pot be assigned to?: ")
                             pot_input = input()
-
                             # Find the pot using a simple loop
                             selected_pot = None
                             for pot in pots.values():
@@ -135,39 +106,27 @@ Welcome to Money Pots, your savings and budgeting calculator. Let me help you to
                                 selected_pot.pot_value()
                                 break
                             else:
-                                print(f"pot '{pot_input}' not found. Please enter a valid pot name.")
-                                print()
+                                print_slow(f"\npot '{pot_input}' not found. Please enter a valid pot name.")
                     break
                 
                 except ValueError as e:  
-                    print(f"Error: {e}")
-                    print()
-
+                    print_slow(f"Error: {e}")
                 except Exception as e:  
-                    print(f"An unexpected error occurred: {e}")
-                    print()
-            
-            print()
-
+                    print_slow(f"An unexpected error occurred: {e}")
+    
         # Print Summary
         elif action == "Summary":
-
             summary(vaults, pots)
-            print()
-
         # Print Instructions
         elif action == "Instructions":
-            print_slow(instructions())
-
+            print_slow_nospace(instructions())
         # Create new Vault
         elif action == "Vault":
             vault_count = count_vaults()
             vaults[f"vault_{(vault_count + 1)}"] = create_vault(vault_count, user)
-            print()
-        
         # Create new Pot
         elif action == "Pot":
-            print_slow("What vault will this pot be assigned to? ")
+            print_slow("\nWhat vault will this pot be assigned to? ")
             pot_vault = input()
             pot_count = count_pots()
             selected_vault = None
@@ -179,21 +138,17 @@ Welcome to Money Pots, your savings and budgeting calculator. Let me help you to
                 pots[f"pot_{(pot_count + 1)}"] = create_pot(pot_count, selected_vault, user)
 
             else:
-                print(f"Vault '{vault_input}' not found. Please enter a valid vault name.")
-                print()
-        
+                print_slow(f"Vault '{vault_input}' not found. Please enter a valid vault name.")
+                
         # Exit
         elif action == "Exit":
-            print_slow("OK, the program will now terminate. See final values of the vaults and pots below. Thanks for using Money Pots!")
-            print()
+            print_slow("\nOK, the program will now terminate. See final values of the vaults and pots below. Thanks for using Money Pots!")
             summary(vaults, pots)
-            print()
+            # Close the database connections
+            con.close()
             exit()
-
         else:
-            print_slow("Invalid command. Please try again")
-            print()
-        
-
+            print_slow_nospace("\nInvalid command. Please try again")
+            
 if __name__ == "__main__":
     main()
--- project_functions.py
+++ project_functions.py
@@ -3,44 +3,31 @@ from project_classes import User, Vault, Pot, Transaction
 from time import sleep
 
 def submit_transaction(x, pot, user):
-    
     # Collect transaction name
-    print()
-    print_slow("Please provide a name reference for this transaction: ")
+    print_slow("\nPlease provide a name reference for this transaction: ")
     transaction_name = input()
-    print()
-
     # Collect transaction id
     transaction_id = x + 1
-
     # Collect date data and create date object
-    print_slow("Excellent. Now we'll define when the transaction took place. Please note, all date input values must be in the format DD/MM/YY")
-    print()
-    print()
-
+    print_slow("\nExcellent. Now we'll define when the transaction took place. Please note, all date input values must be in the format DD/MM/YY")
     date = collect_date("Date of transction: ")
-
     # Collect transaction type
-    print()
-    
     while True:
         types = ["in", "out"]
-        print_slow('Please define the type of transaction. "in" or "out": ')
-        type = input()
-        if type not in types:
-            print("incorrect transaction reference")
+        print_slow('\nPlease define the type of transaction. "in" or "out": ')
+        transaction_type = input()
+        if transaction_type not in types:
+            print_slow_nospace("\nincorrect transaction reference")
         else:
             break
-    
     # Collect transaction amount
-    print()
-    print_slow("What is the transaction amount?: ")
+    print_slow("\nWhat is the transaction amount?: ")
     while True:
         amount = int_validator()
         if amount > 0:
             break
         else:
-            print("amount must be greater than 0")
+            print_slow("\namount must be greater than 0")
         
     if type == "out":
         amount = amount * -1
@@ -48,12 +35,20 @@ def submit_transaction(x, pot, user):
         pass
 
     #Input all information into the Class
-    transaction = Transaction(transaction_id=transaction_id, transaction_name=transaction_name, date=date, pot=pot, type=type, amount=amount, user=user)
-    
+    transaction = Transaction(transaction_id=transaction_id, transaction_name=transaction_name, date=date, pot=pot, type=transaction_type, amount=amount, user=user)
+    transaction_data = [(transaction_id, transaction_name, date, pot.pot_id, transaction_type, amount, user.username)]
     if transaction:
-        print_slow("Thanks, your transaction has been created succesfully")
+        transaction_data
+        print_slow("\nThanks, your transaction has been created succesfully")
         # save transaction to database
-        print()
+        # Establish a connection to the Database
+        db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
+        con = sqlite3.connect(db_path)
+        cur = con.cursor()
+        # Save transaction to database
+        cur.executemany("INSERT INTO transactions VALUES(?, ?, ?, ?, ?, ?, ?)", transaction_data)
+        con.commit()
+        con.close()
     else:
         print_slow("ERROR: transaction not created succesfully")
     
@@ -63,15 +58,21 @@ def print_slow(txt):
     for x in txt: 
         print(x, end='', flush=True)
         sleep(0) #0.025 at end of programme
+    print("",end="\n\n")
+
+def print_slow_nospace(txt):
+    for x in txt: 
+        print(x, end='', flush=True)
+        sleep(0) #0.025 at end of programme
+    print("",end="\n")
 
 def int_validator():
     while True:
         try:
             value = int(input())
-            print()
             break
         except ValueError:
-            print_slow("Invalid input. Please enter a valid integer: ")
+            print_slow("\nInvalid input. Please enter a valid integer: ")
         
     return value
 
@@ -83,7 +84,7 @@ def collect_date(message):
             date = datetime.datetime.strptime(date_input,"%d/%m/%y")
             break 
         except ValueError as err:
-            print(err)
+            print_slow(f"\n{str(err)}")
     return date
 
 def convert_date(string):
@@ -91,30 +92,28 @@ def convert_date(string):
         date_input = string
         date = datetime.datetime.strptime(date_input,"%Y-%m-%d %H:%M:%S")
     except ValueError as err:
-        print(err)
+        print_slow(f"\n{str(err)}")
 
     return date
 
 def summary(vaults, pots):
-
     for i in vaults:
-        print()
-        print_slow(f"\033[31mVault\033[0m") 
-        print()
-        print_slow(f"{vaults[i].vault_name} = $")
+        print_slow_nospace(f"\n\033[31mVault\033[0m") 
         vault_value = vaults[i].vault_value()
-        print_slow(f"{vault_value}")
-        print()
-        print_slow(f"\033[31mPots\033[0m") 
-        print()
+        print_slow_nospace(f"{vaults[i].vault_name} = ${vault_value}")
+        print_slow_nospace(f"\033[31mPots\033[0m") 
+        
         for j in pots:
                 if pots[j].vault == vaults[i]:
-                    print_slow(f"{pots[j].pot_name} = ${pots[j].amount}")
-                    print()
+                    print_slow_nospace(f"{pots[j].pot_name} = ${pots[j].amount}")
                 else:
                     continue
 
 def create_user(*args):
+    # Establish a connection to the Database
+    db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
     if args:
         username = args[0]
         user = User(username)
@@ -123,109 +122,92 @@ def create_user(*args):
         username = input()
         user = User(username)
         # save user to database
-        print("")
+        users_data = [
+        (user.username,),
+        ]
+        cur.executemany("INSERT INTO users VALUES(?)", users_data)
+        con.commit()
+        con.close()
     return user
 
 def create_pot(x, vault, user):
-    
+    # Establish a connection to the Database
+    db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
     # Collect pot name
-    print()
-    print_slow("What is your preferred name for the pot?: ")
+    print_slow("\nWhat is your preferred name for the pot?: ")
     pot_name = input()
-    print()
-
     # Collect pot id
     pot_id = x + 1
-
     # Collect start date data and create date object
-    print_slow("Excellent. Now we'll define when the pot will be in use. Please note, all date input values must be in the format DD/MM/YY")
-    print()
-    print()
-
+    print_slow("\nExcellent. Now we'll define when the pot will be in use. Please note, all date input values must be in the format DD/MM/YY")
     start_date = collect_date("What is the start date that this pot will be active?: ")
-
     # Collect end date data and create date object
-    print()
-    end_date = collect_date("What is the end date that this pot will be active?: ")
-
+    end_date = collect_date("\nWhat is the end date that this pot will be active?: ")
     # Collect pot amount
-    print()
-    print_slow("What is the amount of money in the pot?: ")
+    print_slow("\nWhat is the amount of money in the pot?: ")
     while True:
         amount = int_validator()
         if amount > 0:
             break
         else:
-            print("amount must be greater than 0") 
+            print_slow("\namount must be greater than 0") 
 
     #Input all information into the Class
     pot = Pot(pot_id=pot_id, pot_name=pot_name, start=start_date, end=end_date, vault=vault, amount=amount, user=user)
     
     if pot:
-        print_slow("Thanks, your pot has been created succesfully")
+        print_slow_nospace("\nThanks, your pot has been created succesfully")
+
         # save pot to database
-        print()
-        print()
+        pots_data = []
+        pots_data.append((pot.pot_id, pot.pot_name, pot.start, pot.end, pot.vault_id, pot.amount, pot.username))
+            
+        cur.executemany("INSERT INTO pots VALUES(?, ?, ?, ?, ?, ?, ?)", pots_data)
+        con.commit()
+        con.close()
     else:
-        print_slow("ERROR: pot not created succesfully")
+        print_slow("\nERROR: pot not created succesfully")
     
     return pot
 
 def create_vault(x, user):
-    
-    # Collect vault name
-    print()
-    print_slow("What is your preferred name for the vault?: ")
-    vault_name = input()
-    print()
-
-    # Collect vault id
-    vault_id = x + 1
-
-    # Collect start date data and create date object
-    print_slow("Excellent. Now we'll define when the vault will be in use. Please note, all date input values must be in the format DD/MM/YY")
-    print()
-    print()
-
-    start_date = collect_date("What is the start date that this vault will be active?: ")
-
-    # Collect end date data and create date object
-    print()
-    end_date = collect_date("What is the end date that this vault will be active?: ")
-
-
-    #Input all information into the Class
-    vault = Vault(vault_id=vault_id, vault_name=vault_name, start=start_date, end=end_date, user=user)
-    
-    if vault:
-        print()
-        print_slow("Thanks, your vault has been created succesfully")
-        # save vault to database
-        print()
-        print()
-    else:
-        print_slow("ERROR: vault not created succesfully")
-    
-    return vault
-
-def create_profile():
     # Establish a connection to the Database
     db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-    
+    # Collect vault name
+    print_slow("\nWhat is your preferred name for the vault?: ")
+    vault_name = input()
+    # Collect vault id
+    vault_id = x + 1
+    # Collect start date data and create date object
+    print_slow("\nExcellent. Now we'll define when the vault will be in use. Please note, all date input values must be in the format DD/MM/YY")
+    start_date = collect_date("What is the start date that this vault will be active?: ")
+    # Collect end date data and create date object
+    end_date = collect_date("\nWhat is the end date that this vault will be active?: ")
+    #Input all information into the Class
+    vault = Vault(vault_id=vault_id, vault_name=vault_name, start=start_date, end=end_date, user=user)
+    if vault:
+        print_slow_nospace("\nThanks, your vault has been created succesfully")
+        # save vault to database
+        vaults_data = []
+        vaults_data.append((vault.vault_id, vault.vault_name, vault.start, vault.end, vault.username))
+        cur.executemany("INSERT INTO vaults VALUES(?, ?, ?, ?, ?)", vaults_data)
+        con.commit()
+        con.close()
+    else:
+        print_slow("\nERROR: vault not created succesfully")
+    return vault
+
+def create_profile():
     # Create a User object
-    print()
     user = create_user()
-
     # Count number of existing vaults in database. if not exist = 0
-    res = cur.execute("SELECT vault_id FROM vaults")
-    start_vault = len(res.fetchall())
-
+    start_vault = count_vaults()
     # Count number of existing pots in database. if not exist = 0
-    res = cur.execute("SELECT pot_id FROM pots")
-    start_pot = len(res.fetchall())
-
+    start_pot = count_pots()
     # Create a Vault object with valid data
     print_slow(f"Hi {user.username}, let me help you create some vaults. How many do you want to create?: ")
     no_vaults = int_validator()
@@ -233,14 +215,14 @@ def create_profile():
     
     try:
         for x in range(no_vaults):
-            print(f"Vault {(x+1)+start_vault}")
+            print_slow(f"Vault {(x+1)+start_vault}")
             vaults["vault_{0}".format((x+1)+start_vault)] = create_vault((x+start_vault), user)
     
     except ValueError as e:  
-        print(f"Error: {e}")
+        print_slow(f"Error: {e}")
 
     except Exception as e:  
-        print(f"An unexpected error occurred: {e}")
+        print_slow(f"An unexpected error occurred: {e}")
 
     # Create Pot objects with valid data
     print_slow("Now, let me help you create some pots. How many do you want to create?: ")
@@ -250,8 +232,7 @@ def create_profile():
     while True:
         try:
             for x in range(no_pots):
-                print(f"Pot {(x+1)+start_pot}")
-                print()
+                print_slow(f"Pot {(x+1)+start_pot}")
                 
                 while True: 
                     print_slow("What vault should this pot be assigned to?: ")
@@ -268,49 +249,19 @@ def create_profile():
                         pots[f"pot_{(x+1)+start_pot}"] = create_pot((x+start_pot), selected_vault, user)
                         break
                     else:
-                        print(f"Vault '{vault_input}' not found. Please enter a valid vault name.")
-                        print()
+                        print_slow(f"Vault '{vault_input}' not found. Please enter a valid vault name.")
+                        
             break
         
         except ValueError as e:  
-            print(f"Error: {e}")
-            print()
-
+            print_slow(f"Error: {e}")
+            
         except Exception as e:  
-            print(f"An unexpected error occurred: {e}")
-            print()
-
+            print_slow(f"An unexpected error occurred: {e}")
+            
     # Summary of the vaults and pots values
     print_slow("See below list of vaults and their summed values")
-    print()
     summary(vaults, pots)
-    print()
-
-    # Insert user data into the database
-    users_data = [
-    (user.username,),
-    ]
-
-    cur.executemany("INSERT INTO users VALUES(?)", users_data)
-
-    # Insert vaults data into the database
-    vaults_data = []
-    for vault in vaults.values():
-        vaults_data.append((vault.vault_id, vault.vault_name, vault.start, vault.end, vault.username))
-        
-    cur.executemany("INSERT INTO vaults VALUES(?, ?, ?, ?, ?)", vaults_data)
-
-    # Insert pots data into the database
-    pots_data = []
-    for pot in pots.values():
-        pots_data.append((pot.pot_id, pot.pot_name, pot.start, pot.end, pot.vault_id, pot.amount, pot.username))
-        
-    cur.executemany("INSERT INTO pots VALUES(?, ?, ?, ?, ?, ?, ?)", pots_data)
-    
-    # Close the database connections
-    con.commit()
-    con.close()
-
     return user, vaults, pots
 
 def instructions():
@@ -339,25 +290,19 @@ the program closes.
 Please note: This program does not store data permanently. Once you exit, all 
 Vault and Pot data will be lost.
 
-We hope you enjoy using Money Pots!
-
-"""
+We hope you enjoy using Money Pots!"""
 
 def re_vaults(name, user):
-
     # Establish Database Connection
     db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-
     # Create vault and vault_ids variables
     vaults = {}
     vault_ids = []
-
     # Searcb the vaults database for all information for defined user 
     res = cur.execute("SELECT * FROM vaults WHERE username = ?", (name,))
     returned_vaults = res.fetchall()
-    
     for vault in returned_vaults:
         # Create variables
         vault_ids.append(int(vault[0]))
@@ -365,33 +310,26 @@ def re_vaults(name, user):
         vault_name = vault[1]
         start_date = convert_date(vault[2])
         end_date = convert_date(vault[3])
-
         # Create vault instance
         vault = Vault(vault_id=vault_id, vault_name=vault_name, start=start_date, end=end_date, user=user)
         # Add instance to vaults object dictionary
         vaults["vault_{0}".format(vault_id)] = vault
-
+    
     con.close()
-
     return vaults, vault_ids
 
 def re_pots(vaults, vault_ids, user):
-
     # Establish Database Connection
     db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-
     # Create pots and pot_id variables
     pots = {}
     pot_ids = []
-
     # Searcb the vaults database for all information for defined vault_ids
     for vault in vault_ids:
-    
         res = cur.execute("SELECT * FROM pots WHERE vault_id = ?", (vault,))
         returned_pots = res.fetchall()
-        
         for pot in returned_pots:
             # Create variables
             pot_id = int(pot[0])
@@ -408,27 +346,20 @@ def re_pots(vaults, vault_ids, user):
             pot_ids.append(pot_id)
 
     con.close()
-
     return pots, pot_ids
 
 def re_transactions(pots, pot_ids, user):
-
     # Establish Database Connection
     db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-
     # Create pots and transaction_id variables
     transactions = {}
     transaction_ids = []
-
     # Searcb the pots database for all information for defined pot_ids
-
     for pot in pot_ids:
-    
         res = cur.execute("SELECT * FROM transactions WHERE pot_id = ?", (pot,))
         returned_transactions = res.fetchall()
-
         for transaction in returned_transactions:
             # Create variables
                 transaction_id = int(transaction[0])
@@ -443,9 +374,7 @@ def re_transactions(pots, pot_ids, user):
                 transactions[f"transaction_{transaction.transaction_id}"] = transaction
                 # Append transaction_id to list
                 transaction_ids.append(transaction_id)
-
     con.close()
-
     return transactions, transaction_ids
 
 def count_pots():
@@ -453,24 +382,20 @@ def count_pots():
     db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-
     # Search the database
     res = cur.execute("SELECT * FROM pots")
     returned_pots = res.fetchall()
-
     # Calculate Length of returned pots
     return len(returned_pots)
-        
+    
 def count_vaults():
     # Establish Database Connection
     db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-
     # Search the database
     res = cur.execute("SELECT * FROM vaults")
     returned_vaults = res.fetchall()
-
     # Calculate Length of returned pots
     return len(returned_vaults)
         
@@ -479,10 +404,8 @@ def count_transactions():
     db_path = "/Users/michaelfortune/Developer/projects/money/money_sql/money.db" 
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-
     # Search the database
     res = cur.execute("SELECT * FROM transactions")
     returned_transactions = res.fetchall()
-
     # Calculate Length of returned pots
     return len(returned_transactions)
